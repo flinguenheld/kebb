a1fb8f73b1cec528973c9b20d876115f1c459013
Florent <f@linguenheld.fr>
2023-07-15 15:16:43 +0200

    WidgetList correct geometry

--- src/widget/button/widget_list.cpp
+++ src/widget/button/widget_list.cpp
@@ -10,56 +10,51 @@
 WidgetList::WidgetList(point pos_center, boxsize size_char, std::string &&text,
                        std::vector<std::string> &&choices, bool selected)
     : WidgetSelection(pos_center, size_char, std::move(text), selected), _choices(std::move(choices)),
-      _it(_choices.begin()), _size_char(size_char) {
+      _it(_choices.begin()), _size_char(size_char), _longest_choice_width(0), _space(0) {
 
-  init();
+  init(pos_center);
 }
 
 WidgetList::WidgetList(point pos_center, boxsize size_char, std::string &&text, int16_t range_start,
                        int16_t range_stop, int16_t step, bool selected)
 
-    : WidgetSelection(pos_center, size_char, std::move(text), selected), _size_char(size_char) {
+    : WidgetSelection(pos_center, size_char, std::move(text), selected), _size_char(size_char),
+      _longest_choice_width(0), _space(0) {
 
   for (; range_start <= range_stop; range_start += step)
     _choices.emplace_back(std::to_string(range_start));
 
   _it = _choices.begin();
 
-  init();
+  init(pos_center);
 }
 
 WidgetList::~WidgetList() {}
 
-void WidgetList::init() {
-  // ------------------------------------------------------------------------
-  // Geometry ---------------------------------------------------------------
+void WidgetList::init(point pos_center) {
 
-  // Take the longest word
-  uint16_t longest_word = 0;
-  for (const auto &txt : _choices)
-    longest_word = txt.length() > longest_word ? txt.length() : longest_word;
+  // Geometry --
+  // Find the longest word
+  for (const auto &txt : _choices) {
+    if (txt.length() * _size_char.w > _longest_choice_width)
+      _longest_choice_width = txt.length() * _size_char.w;
+  }
 
   _space = _size_char.w * 2;
-  _position.x -= (_size.w + _space + longest_word) / 2;
-  // FIX: _position has to be recalculate
 
-  _size_choice_longest.w = _size_char.w * longest_word; // Ajustable
-  _size_choice_longest.h = _size_char.h;
+  // Set the global position with the widest choice
+  _position.x = pos_center.x - (_size.w + _space + _longest_choice_width) / 2;
 
-  boxsize size_choices = {static_cast<uint16_t>(_size_char.w * longest_word), _size_char.h};
-
-  _pt_choice_longest.x = _position.x + _size.w + _space + _size_choice_longest.w / 2;
-  _pt_choice_longest.y = _position.y;
-
-  _tb_choice = std::make_unique<WidgetTextBox>(_pt_choice_longest, size_choices);
+  // The _tb_choice position and width will be updated by display_current_it()
+  boxsize size_choices = {_longest_choice_width, _size_char.h};
+  _tb_choice = std::make_unique<WidgetTextBox>(_position, size_choices);
   _tb_choice->set_color_text({148, 226, 213, 200}); // Teal
-
   display_current_it();
 }
 
 // ------------------------------------------------------------------------
 // Actions ----------------------------------------------------------------
-std::string WidgetList::current_selection() const { return *_it; } // TODO: add return
+std::string WidgetList::current_selection() const { return *_it; }
 void WidgetList::set_selection(const std::string &text) {
 
   _it = std::find(_choices.begin(), _choices.end(), text);
@@ -93,17 +88,13 @@ void WidgetList::display_current_it() {
   uint16_t new_width = new_choice.length() * _size_char.w;
 
   _tb_choice->set_w(new_width);
-  _tb_choice->set_x(_position.x + _size.w + _space + (_size_choice_longest.w - new_width) / 2);
+  _tb_choice->set_x(_position.x + _size.w + _space + (_longest_choice_width - new_width) / 2);
   _tb_choice->set_text(std::move(new_choice));
 }
+
 // ------------------------------------------------------------------------
 // Render -----------------------------------------------------------------
 void WidgetList::render(SDL_Renderer *renderer, TTF_Font *font) const {
-
-  // ------------------------------------------------------------------------
-  // Text -------------------------------------------------------------------
-  WidgetTextBox::render(renderer, font);
-  // ------------------------------------------------------------------------
-  // Choices -----------------------------------------------------------------
   _tb_choice->render(renderer, font);
+  WidgetTextBox::render(renderer, font);
 }
--- src/widget/button/widget_list.h
+++ src/widget/button/widget_list.h
@@ -8,13 +8,16 @@
 #include <memory>
 #include <vector>
 
+/*
+ * Based on WidgetSelection, this class add another WidgetTextBox to display an range of strings.
+ * Use action_left/action_right methods to change the current value.
+ */
 class WidgetList : public WidgetSelection {
 public:
   WidgetList(point pos_center, boxsize size_char, std::string &&text, std::vector<std::string> &&choices,
              bool selected = false);
   WidgetList(point pos_center, boxsize size_char, std::string &&text, int16_t range_start, int16_t range_stop,
              int16_t step, bool selected = false);
-
   virtual ~WidgetList();
 
   std::string current_selection() const;
@@ -26,15 +29,14 @@ public:
   virtual void render(SDL_Renderer *renderer, TTF_Font *font) const;
 
 private:
-  void init();
+  void init(point pos_center);
 
   std::vector<std::string> _choices;
   std::vector<std::string>::iterator _it;
   std::unique_ptr<WidgetTextBox> _tb_choice;
   void display_current_it();
 
-  boxsize _size_choice_longest;
-  point _pt_choice_longest;
+  uint16_t _longest_choice_width;
   uint16_t _space;
 
   const boxsize _size_char;
